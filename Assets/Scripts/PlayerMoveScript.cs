5c1f644a204d81fafba1f0785da1f11309251ddb
PaddyMc <paddymchale@hotmail.com>
2017-07-14 20:18:54 +0100

    added player obj and script

---
+++ Assets/Scripts/PlayerMoveScript.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMoveScript : MonoBehaviour {
+    private Rigidbody rb;
+    // Use this for initialization
+    void Start () {
+        //Console.WriteLine("Hello Unity");
+        //System.Console.WriteLine("Hello");
+        //Debug.Log("JPAsf");
+        rb = GetComponent<Rigidbody>();
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    void FixedUpdate()
+    {
+        float moveHorizontal = Input.GetAxis("Horizontal");
+        float moveVertical = Input.GetAxis("Vertical");
+
+        //if (Input.GetButtonDown("Fire1")) {
+        Vector3 movement = new Vector3(moveHorizontal, 0.0f, moveVertical);
+        rb.AddForce(movement * 1);
+        //}
+            
+
+
+        
+    }
+}
