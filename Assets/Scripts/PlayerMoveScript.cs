bbb2b4e8147be86f7eb73d8c7910bf81714c0a23
PaddyMc <paddymchale@hotmail.com>
2017-07-15 12:31:27 +0100

    playerMovement

--- Assets/Scripts/PlayerMoveScript.cs
+++ Assets/Scripts/PlayerMoveScript.cs
@@ -6,15 +6,19 @@ public class PlayerMoveScript : MonoBehaviour {
     private Rigidbody rb;
     private float startAcceleration;
     public float speed = 30.0F;
+    public Transform target;
+    private bool isAbleToTurn;
+    //public float speed;
     // Use this for initialization
     void Start () {
         //Console.WriteLine("Hello Unity");
         //System.Console.WriteLine("Hello");
         //Debug.Log("JPAsf");
         rb = GetComponent<Rigidbody>();
-        Vector3 movement = new Vector3(0.0f, -20.0f, 0.0f);
-        rb.AddForce(movement * 1);
-    }
+        //Vector3 movement = new Vector3(0.0f, -20.0f, 0.0f);
+        //rb.AddForce(movement * 1);
+        
+}
 	
 	// Update is called once per frame
 	void Update () {
@@ -23,30 +27,45 @@ public class PlayerMoveScript : MonoBehaviour {
 
     void FixedUpdate()
     {
-        var isAbleToTurn = false;
-        var direction = 0.0f; 
+        isAbleToTurn = false;
         //var x = Input.GetAxis("Horizontal") * Time.deltaTime * 150.0f;
         //var z = Input.GetAxis("Vertical") * Time.deltaTime * 3.0f;
 
         //transform.Rotate(0, 0, 0);
         //transform.Translate(0, 0, z);
         //xTiltPhoneValue = Input.acceleration.x;
-        var calculatedAngle = transform.eulerAngles.z;
-        if (calculatedAngle >= 90.0f)
-        {
-            //print("Greater than 90");
-            var movement = 360.0f - calculatedAngle;
-            if (movement <= 90.0f)
-            {
-                direction = calculatedAngle;
-                isAbleToTurn = true;
-            }
-        }
-        else {
-            isAbleToTurn = true;
-            direction = calculatedAngle;
-        }
+        float calculatedAngle = transform.eulerAngles.z;
+        float numberOfRotations = calculatedAngle / 360.0f;
+        //print(numberOfRotations);
+        //if (numberOfRotations > 0.1f)
+        //{
+        //    print("full Rotation");
+        //    float adjustedAngleToRotate = calculatedAngle - (360.0f * numberOfRotations);
+        //    print(adjustedAngleToRotate);
+        //}
+        //else {
+        //print(calculatedAngle);
+        //if (calculatedAngle >= 90.0f)
+        //{
 
+        //    var movement = 360.0f - calculatedAngle;
+        //    //print(movement);
+        //    if (movement <= 90.0f)
+        //    {
+        //        print("Greater than 90 but less than 360");
+        //        isAbleToTurn = true;
+        //    }
+        //}
+        //else
+        //{
+        //    print("Less than 90");
+        //    isAbleToTurn = true;
+        //}
+
+        //}
+        float step = speed * Time.deltaTime;
+        transform.position = Vector3.MoveTowards(transform.position, target.position, step);
+        
         //if (calculatedAngle < 90.0f)
         //{
         //    print("Hope");
@@ -69,13 +88,13 @@ public class PlayerMoveScript : MonoBehaviour {
         //dir *= Time.deltaTime;
         //transform.Translate(dir * speed);
         //transform.Rotate(dir * speed);
-        if (isAbleToTurn) {
+        //if (isAbleToTurn) {
             //print("Not able to turn");
-            Vector3 rotation = new Vector3(0.0f, 0.0f, (Input.acceleration.x * 250.0f));
-            transform.Rotate(rotation * Time.deltaTime);
+            Vector3 rotation = new Vector3(0.0f, 0.0f, (Input.acceleration.x * 270.0f));
+            transform.Rotate(rotation * Time.deltaTime);    
             //Vector3 playerMovementDirection = new Vector3(0.0f, -3.0f, 0.0f);
             //transform.Translate(playerMovementDirection * speed);
-        }
+        //}
 
 
 
