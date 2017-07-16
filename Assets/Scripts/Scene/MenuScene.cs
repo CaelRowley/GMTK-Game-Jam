c58a5f6176d382961d9b91c172eeac2a6864a17b
CaelRowley <caelrowley@gmail.com>
2017-07-16 11:44:48 +0100

    Added main menu and preloader scene

---
+++ Assets/Scripts/Scene/MenuScene.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MenuScene : MonoBehaviour {
+
+    private CanvasGroup fadeGroup;
+    private float fadeInSpeed = 0.33f;
+
+    private void Start() {
+        fadeGroup = FindObjectOfType<CanvasGroup>();
+
+        fadeGroup.alpha = 1;
+    }
+
+
+    private void Update() {
+        fadeGroup.alpha = 1 - Time.timeSinceLevelLoad * fadeInSpeed;
+    }
+
+
+    public void OnStartClick() {
+        print("pressed");
+        SceneManager.LoadScene("GameScene");
+    }
+}
---
+++ Assets/Scripts/Scene/Preloader.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Preloader : MonoBehaviour {
+
+    private CanvasGroup fadeGroup;
+    private float loadTime;
+    public float logoTimeMin = 3.0f;
+
+    private void Start() {
+        fadeGroup = FindObjectOfType<CanvasGroup>();
+
+        fadeGroup.alpha = 1;
+
+        if(Time.time < logoTimeMin)
+            loadTime = logoTimeMin;
+        else
+            loadTime = Time.time;
+    }
+
+
+    private void Update() {
+        if(Time.time < logoTimeMin) {
+            fadeGroup.alpha = 1 - Time.time;
+        }
+
+        if(Time.time > logoTimeMin && loadTime != 0) {
+            fadeGroup.alpha = Time.time - logoTimeMin;
+            if(fadeGroup.alpha >= 1) {
+                SceneManager.LoadScene("MainMenu");
+            }
+        }
+    }
+}
