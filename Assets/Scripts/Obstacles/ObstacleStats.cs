8701808a6ca4c487c76a37e84b0eb4f267ba332d
CaelRowley <caelrowley@gmail.com>
2017-07-16 15:36:40 +0100

    Fixed master branch

--- Assets/Scripts/Obstacles/ObstacleStats.cs
+++ Assets/Scripts/Obstacles/ObstacleStats.cs
@@ -5,11 +5,27 @@ using UnityEngine;
 public class ObstacleStats : MonoBehaviour {
     public int health;
     public GameObject explosion;
+    public bool invincible = true;
+    public GameObject player;
+    public float invincibilityOffset;
+
+    private void Start() {
+        player = GameObject.FindGameObjectWithTag("Player");
+    }
+
 
     private void Update() {
         if(health <= 0) {
-            GameObject newExplosion = Instantiate(explosion, transform.position, transform.rotation) as GameObject;
-            Destroy(gameObject);
+            if(invincible)
+                health = 1;
+            else {
+                GameObject newExplosion = Instantiate(explosion, transform.position, transform.rotation) as GameObject;
+                Destroy(gameObject);
+            }
+        }
+
+        if(transform.position.y > player.transform.position.y + invincibilityOffset) {
+            invincible = false;
         }
     }
 }
