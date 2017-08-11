131545ecbadec3e4af284da12ce3a660c5975cef
PaddyMc <paddymchale@hotmail.com>
2017-08-11 20:21:50 +0100

    async game loading

--- Assets/Scripts/Scene/MenuScene.cs
+++ Assets/Scripts/Scene/MenuScene.cs
@@ -7,23 +7,25 @@ public class MenuScene : MonoBehaviour {
 
     private CanvasGroup fadeGroup;
     private float fadeInSpeed = 1f;
+    AsyncOperation loadingScene;
 
     private void Start() {
         fadeGroup = FindObjectOfType<CanvasGroup>();
-
+        loadingScene = SceneManager.LoadSceneAsync("GameScene");
+        loadingScene.allowSceneActivation = false;
         fadeGroup.alpha = 1;
-
     }
 
 
     private void Update() {
         fadeGroup.alpha = 1 - Time.timeSinceLevelLoad * fadeInSpeed;
+        //print(loadingScene.progress);
+        
     }
-
 
     public void OnStartClick() {
         fadeGroup.alpha = 1;
+        loadingScene.allowSceneActivation = true;
         print("pressed");
-        SceneManager.LoadScene("GameScene");
     }
 }
