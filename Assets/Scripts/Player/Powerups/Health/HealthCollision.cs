d0fd08efaa162441881ec6e6413b4d23c322f042
CaelRowley <caelrowley@gmail.com>
2017-08-20 20:35:46 +0100

    Created health pickup ship

---
+++ Assets/Scripts/Player/Powerups/Health/HealthCollision.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HealthCollision : MonoBehaviour {
+
+    void Start() {
+        GameObject player = GameObject.FindGameObjectWithTag("Player");
+        PlayerStats playerStats = player.gameObject.GetComponent<PlayerStats>();
+        playerStats.health += 2;
+        Debug.Log(playerStats.health);
+        Destroy(gameObject);
+    }
+}
