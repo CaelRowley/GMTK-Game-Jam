b29022ed5db861eece20d9eb471e5af6499b051d
CaelRowley <caelrowley@gmail.com>
2017-10-27 22:55:05 +0100

    Updated the player and added new shield animation

--- Assets/Scripts/Player/Powerups/Forcefield/ForcefieldStats.cs
+++ Assets/Scripts/Player/Powerups/Forcefield/ForcefieldStats.cs
@@ -6,7 +6,16 @@ public class ForcefieldStats : MonoBehaviour {
 
     public int health;
 
+    private GameObject player;
+
+    void Start() {
+        player = GameObject.FindGameObjectWithTag("Player");
+    }
+
     private void Update() {
+        transform.position = new Vector3(player.transform.position.x, player.transform.position.y, player.transform.position.z);
+        transform.rotation = player.transform.rotation;
+
         if(health <= 0) {
             Destroy(gameObject);
         }
